56f644c45bfd6d98e2908137d5d69f016c3c3f9d
Zhang WENG <zweng@e1r12p5.clusters.42paris.fr>
2022-11-15 15:18:38 +0100

    updates

--- srcs/main.c
+++ srcs/main.c
@@ -6,12 +6,39 @@
 /*   By: zweng <zweng@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 12:23:00 by zweng             #+#    #+#             */
-/*   Updated: 2022/11/02 12:24:02 by zweng            ###   ########.fr       */
+/*   Updated: 2022/11/13 17:53:08 by zweng            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_nm.h"
 
-int main() {
-    return 0;
+static int  init_params(t_param *params, int ac, char **av)
+{
+    int     i;
+
+    i = 1;
+    while (i < ac && av[i][0] == '-')
+    {
+
+    }
+}
+
+int         main(int ac, char **av) {
+    int     i;
+    int     ret;
+    t_param params
+
+    ret = 0;
+    i = init_params(&params, ac, av);
+    if (ac - i <= 1)
+        ret = ft_nm("a.out", params);
+    else
+    {
+        while (i < ac)
+        {
+           ret += ft_nm(av[i], params);
+           i++;
+        }
+    }
+    return (ret);
 }
